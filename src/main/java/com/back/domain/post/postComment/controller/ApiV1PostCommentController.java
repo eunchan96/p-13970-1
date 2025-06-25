33180254a4a1c2dc7315c4e6d737d4ea41a37408
eunchan96 <taegwon02@gmail.com>
2025-06-25 15:41:57 +0900

    Map(delete msg)

--- src/main/java/com/back/domain/post/postComment/controller/ApiV1PostCommentController.java
+++ src/main/java/com/back/domain/post/postComment/controller/ApiV1PostCommentController.java
@@ -6,9 +6,14 @@ import com.back.domain.post.postComment.dto.PostCommentDto;
 import com.back.domain.post.postComment.entity.PostComment;
 import lombok.RequiredArgsConstructor;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/posts/{postId}/comments")
@@ -36,12 +41,16 @@ public class ApiV1PostCommentController {
 
     @GetMapping("/{id}/delete")
     @Transactional
-    public String delete(@PathVariable int postId, @PathVariable int id) {
+    public Map<String, Object> delete(@PathVariable int postId, @PathVariable int id) {
         Post post = postService.findById(postId).get();
         PostComment postComment = post.findCommentById(id).get();
 
         postService.deleteComment(post, postComment);
 
-        return "%d번 댓글이 삭제되었습니다.".formatted(id);
+        Map<String, Object> rsData = new LinkedHashMap<>();
+        rsData.put("resultCode", "200-1");
+        rsData.put("msg", "%d번 댓글이 삭제되었습니다.".formatted(id));
+
+        return rsData;
     }
 }
