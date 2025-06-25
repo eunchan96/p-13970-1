d53fad5b59a9ee96432059d81c04adca5633aeab
eunchan96 <taegwon02@gmail.com>
2025-06-25 14:41:32 +0900

    getItem, PostDto

--- src/main/java/com/back/domain/post/post/controller/ApiV1PostController.java
+++ src/main/java/com/back/domain/post/post/controller/ApiV1PostController.java
@@ -1,13 +1,16 @@
 package com.back.domain.post.post.controller;
 
+import com.back.domain.post.post.dto.PostDto;
 import com.back.domain.post.post.entity.Post;
 import com.back.domain.post.post.service.PostService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/v1/posts")
@@ -16,9 +19,17 @@ public class ApiV1PostController {
     private final PostService postService;
 
     @GetMapping
-    public List<Post> getItems() {
+    public List<PostDto> getItems() {
         List<Post> items = postService.findAll();
 
-        return items;
+        return items.stream()
+                .map(post -> new PostDto(post))
+                .collect(Collectors.toList());
+    }
+
+    @GetMapping("/{id}")
+    public PostDto getItem(@PathVariable int id) {
+        Post post = postService.findById(id).get();
+        return new PostDto(post);
     }
 }
