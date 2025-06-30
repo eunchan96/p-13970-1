65348d376da89f20c4c391af7907b796a03dd17a
eunchan96 <taegwon02@gmail.com>
2025-06-30 14:32:46 +0900

    Open API

--- build.gradle.kts
+++ build.gradle.kts
@@ -38,6 +38,8 @@ dependencies {
 
     compileOnly("org.projectlombok:lombok")
     annotationProcessor("org.projectlombok:lombok")
+
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.9")
 }
 
 tasks.withType<Test> {
